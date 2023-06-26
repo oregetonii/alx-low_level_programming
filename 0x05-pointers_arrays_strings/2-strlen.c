ba956547047ac4f72324115ccc70528d8002ec2a
orege <proffzion30@gmail.com>
2023-06-26 16:55:29 -0700

    C pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,6 +2,7 @@
 
 /**
  * _strlen - takes a pointer to char and returns length
+ * @s: pointer to string
  * Return: 0
  */
 
@@ -10,6 +11,7 @@ int _strlen(char *s)
 	int count = 0;
 	char  c;
 	s = &c;
+
 	while (*s != '\0')
 	{
 		s++;
