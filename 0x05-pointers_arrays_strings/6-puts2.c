77ac9042759b79531235b5761dfb57ac04ee4968
orege <proffzion30@gmail.com>
2023-06-26 17:48:41 -0700

    C pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,22 +2,18 @@
 
 /**
  * puts2 - prints every other character of a string to the stdio followed by new line
- * @*s: pointer to the address of the string
- * Retuor: 0
+ * @str: pointer to the address of the string
  */
 
 void puts2(char *str)
 {
-        char c;
-        char *s = &c;
-
-        while (*s != '\0')
+        while (*str != '\0')
         {
-                _putchar(*s);
-                s++;
-		s++;
+                _putchar(*str);
+                str++;
+		str++;
         }
+
         _putchar('\n');
 
-        return (0);
 }
