4c3264e2403b9db45c6d47422e2308d5f63961ae
orege <proffzion30@gmail.com>
2023-07-05 11:04:39 -0700

    C_Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: string
+ */
+
+void _puts_recursion(char *s);
+
+
+#endif
