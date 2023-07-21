94bb7428b1996792ebd4d0c29df6df40c51c3011
orege <proffzion30@gmail.com>
2023-07-21 05:52:25 -0700

    Pointers and Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,28 +13,28 @@
  */
 int _strlen(char *s);
 
-int _strlen(char *s)
-{
-	int count = 0;
-        while (*s != '\0')
-        {
-		count++;
-                s++;
-        }
-        return (count);
-}
-
 void print_rev(char *s)
 {
 	int len;
 	char *ss;
 	len = _strlen(s);
-	ss = s + (len - 1);
 
+	ss = s + (len - 1);
 	while (ss >= s)
 	{
 		_putchar(*ss);
 		ss--;
 	}
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
 }
