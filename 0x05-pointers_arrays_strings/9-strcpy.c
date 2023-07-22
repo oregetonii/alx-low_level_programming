2299d3eec859060a1940589c80e4272ee5c21883
orege <proffzion30@gmail.com>
2023-07-22 02:09:33 -0700

    C pointers and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,8 +7,32 @@
  *Return: dest
  */ 
 
+int _strlen(char *s);
+
 char *_strcpy(char *dest, char *src)
 {
-	*dest = *src;
+	int i;
+	int count = _strlen(src);
+
+	for (i = 0; i <= count + 1; i++)
+		dest[i] = src[i];
 	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: int value
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
 }
