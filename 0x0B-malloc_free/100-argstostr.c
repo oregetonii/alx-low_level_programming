f351ccb616c9abb974554fe5f96c47a9bf8d57ef
orege <proffzion30@gmail.com>
2023-07-28 11:08:29 -0700

    betty check

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,24 +29,19 @@ char *argstostr(int ac, char **av)
 {
 	char *s;
 	int len = 0, i, j, k = 0;
-
 	if (ac == 0 || av == NULL) 
 		return (NULL);
-	
 	for (i = 0; i < ac; i++)
 	{
 		len += _strlen(av[i]);
 	}
 	len += (ac + 1); 
-	
 	s = malloc(len * sizeof(char));
-
 	if (s == NULL)
 	{
 		free(s);
 		return (NULL);
 	}
-	
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; j < _strlen(av[i]); j++)
