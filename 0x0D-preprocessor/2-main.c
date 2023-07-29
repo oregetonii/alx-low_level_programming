fa7c034c5c62269d6bba1126b20fd48e6886ab70
orege <proffzion30@gmail.com>
2023-07-29 11:09:47 -0700

    macro expansion

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+/**
+ * main - prints the current executing
+ * file to stdio
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - prints the name of the file
+ * from which it is compiled
+ * Return: 0
+ */
+
+int main(void);
+
+#endif
