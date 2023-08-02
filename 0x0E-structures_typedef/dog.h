74af1062861c5ece02bf73759c9961f530ecb2c3
orege <proffzion30@gmail.com>
2023-08-02 02:04:45 -0700

    inti commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Creates anew datatype dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: creates new structure of type dog,
+ * with three members; name, age, and owner
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+#endif
