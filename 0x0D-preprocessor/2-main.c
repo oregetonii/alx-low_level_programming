3f75c7ce73981cd756ce48e8530855efd7defdb2
oregetonii <proffzion30@gmail.com>
2023-10-09 20:21:04 +0000

    attempt one

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#define ABS(x) ((x) < (0) ? (0 - x) : (x))
+
+#endif
