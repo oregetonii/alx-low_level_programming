18ab7c7a6922449d7879b67858e17779777fdab8
oregetonii <proffzion30@gmail.com>
2023-10-31 07:04:24 +0000

    attempt twelve

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,11 +9,15 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd; 
+	struct stat f;
 
 	if (! filename)
 		exit(-1);
-	fd = open(filename, O_WRONLY | O_APPEND | O_EXCL);
+	fd = open(filename, O_RDWR | O_APPEND | O_EXCL);
 	if (fd == -1)
+		exit(-1);
+	stat(filename, &f);
+	if (! (f.st_mode & S_IRGRP) && (f.st_mode & S_IWGRP))
 		exit(-1);
 	if (text_content != NULL)
 	{
