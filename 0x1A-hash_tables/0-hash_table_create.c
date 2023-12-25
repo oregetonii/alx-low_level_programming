ccb9d61eff8f487fb02a75b555f939499d3620fc
oregetonii <proffzion30@gmail.com>
2023-12-25 09:42:02 +0000

    creating a hash table correction1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,12 +3,14 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: the size of the hash table
+ * Return: pointer to the table if successful
+ * else null
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *head;
 
-	head = malloc(sizeof(hash_node_t) * size);
+	head = malloc(sizeof(hash_table_t) * size);
 	if (head == NULL)
 		return (NULL);
 	else
